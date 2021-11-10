f1a985987c652a19485267c35fd0d50f22faf239
sabhinav10 <sabhinav654@gmail.com>
2021-11-10 21:53:07 +0530

    used minions api and added errorhandler method

--- app.js
+++ app.js
@@ -2,6 +2,20 @@ let inputEl = document.getElementById("input-el");
 let translateBtn = document.getElementById("translate-btn");
 let output = document.getElementById("output");
 
+let serverUrl = "https://api.funtranslations.com/translate/minion.json";
+
+function getTranslationUrl(text){
+    return serverUrl + "?" + "text=" + text;
+}
+
+function errorHandler(error){
+    console.log("error ocurred",error);
+}
+
 translateBtn.addEventListener("click",function(){
-    output.innerText = inputEl.value +" jasjaj asakxmlal";
+    let inputText = inputEl.value;
+
+    //server called here for processing
+    fetch(getTranslationUrl(inputText)).then(response => response.json())
+    .then(json=>output.textContent = json.contents.translated).catch(errorHandler);
 })
