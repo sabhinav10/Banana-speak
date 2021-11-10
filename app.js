2fa9b736f8ff6fb2345de19b192d7800e8124557
sabhinav10 <sabhinav654@gmail.com>
2021-11-10 20:48:05 +0530

    listening for click event

---
+++ app.js
@@ -0,0 +1,7 @@
+let inputEl = document.getElementById("input-el");
+let translateBtn = document.getElementById("translate-btn");
+let output = document.getElementById("output");
+
+translateBtn.addEventListener("click",function(){
+    output.innerText = inputEl.value +" jasjaj asakxmlal";
+})
