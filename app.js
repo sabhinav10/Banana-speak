f5122a17879ca74b9f584e62450289570e992f44
sabhinav10 <sabhinav654@gmail.com>
2022-09-25 07:14:39 +0530

    Initial setup for html and css
    Initial setup for html and css

--- app.js
+++ app.js
@@ -1,21 +1,21 @@
-let inputEl = document.getElementById("input-el");
-let translateBtn = document.getElementById("translate-btn");
-let output = document.getElementById("output");
+// let inputEl = document.getElementById("input-el");
+// let translateBtn = document.getElementById("translate-btn");
+// let output = document.getElementById("output");
 
-let serverUrl = "https://api.funtranslations.com/translate/minion.json";
+// let serverUrl = "https://api.funtranslations.com/translate/minion.json";
 
-function getTranslationUrl(text){
-    return serverUrl + "?" + "text=" + text;
-}
+// function getTranslationUrl(text){
+//     return serverUrl + "?" + "text=" + text;
+// }
 
-function errorHandler(error){
-    console.log("error ocurred",error);
-}
+// function errorHandler(error){
+//     console.log("error ocurred",error);
+// }
 
-translateBtn.addEventListener("click",function(){
-    let inputText = inputEl.value;
+// translateBtn.addEventListener("click",function(){
+//     let inputText = inputEl.value;
 
-    //server called here for processing
-    fetch(getTranslationUrl(inputText)).then(response => response.json())
-    .then(json=>output.textContent = json.contents.translated).catch(errorHandler);
-})
+//     //server called here for processing
+//     fetch(getTranslationUrl(inputText)).then(response => response.json())
+//     .then(json=>output.textContent = json.contents.translated).catch(errorHandler);
+// })
